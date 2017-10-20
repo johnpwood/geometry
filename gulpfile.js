e7497c7065e899a52a4ca2673f71035fc938d2ce
John Wood <johnpwoodiv@gmail.com>
2017-10-20 16:25:22 +0000

    added gulpfile with some tasks to automate

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      del = require('del');
+
+gulp.task('hello', () => {
+    console.log('howdy');
+});
+
+gulp.task('cleanmacs', () => {
+    del('**/*~').then(p => {
+	console.log('deleted:\n '+ p.join('\n'));
+    });
+    del('**/#*#').then(p => {
+	console.log('deleted:\n '+ p.join('\n'));
+    });
+});
+	
+gulp.task('sass', () => {
+    return gulp.src('view/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('view'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('view/*.scss', ['sass'])
+});
+
