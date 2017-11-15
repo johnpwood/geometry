7b05d5cab50108fc31bee0397da01dc838c7a78f
John Wood <johnpwoodiv@gmail.com>
2017-11-15 16:21:08 +0000

    fix issues

--- auth0config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    domain:'yourprofessor.auth0.com',
-    clientID:"RijhOPR2JkI11sZhuuCZuTvZLXN8GMOw", //auth0
-    clientSecret:"Fo1H_SmJOqOCPKfTtuwK8etstZY2WuyE365ys-Cf-D_gFieG03vCxAcyHk_I5ve6",
-    callbackURL: '/callback'
-}
